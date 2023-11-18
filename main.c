231eae06a2a14eb7c4030ca9c82209dc2e607187
Gustavo S Frehse <gusfrehse@gmail.com>
2023-11-18 16:36:35 -0300

    fim

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define ROOT 0
 
 #include <mpi.h>
 
-
 int main(int argc, char **argv)
 {
-    int my_rank, n_procs;
-    //printf("hello world: %d\n", my_rank);
+    int my_rank;
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
 
-    int *buff = malloc(sizeof(int) *  100);
-
-    if (my_rank == ROOT) {
-        for (int i = 0; i < 100; i++) {
-            buff[i] = i * 10;
-        }
-    } else {
-        for (int i = 0; i < 100; i++) {
-            buff[i] = my_rank;
-        }
-    }
-
-    MPI_Bcast(buff, 100, MPI_INT, ROOT, MPI_COMM_WORLD);
-
-    fprintf(stderr, "my_rank: %d buff[30]: %d\n", my_rank, buff[30]);
+    fprintf(stderr, "%d: before first barrier\n", my_rank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    fprintf(stderr, "%d: after first barrier and before second\n", my_rank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    fprintf(stderr, "%d: after second barrier\n", my_rank);
 
     MPI_Finalize();
     return 0;
--- run.sh
+++ run.sh
@@ -2,14 +2,18 @@
 
 tries=10
 
+cmd="mpirun -n $n ./tsp"
+
 for n in 1 2 3 4
 do
     printf "$n"
-    for i in tries
+    for i in {0..10}
     do
-        /usr/bin/time -f %e -o tempo mpirun -n $n ./tsp < tsp.in > /dev/null
-        TEMPO=$(cat tempo)
+        /usr/bin/time -f %e -o tempo $cmd < tsp.in > /dev/null
+        TEMPO=$(cat ./tempo)
+        rm ./tempo
         printf ", $TEMPO"
     done
     printf "\n"
 done
+

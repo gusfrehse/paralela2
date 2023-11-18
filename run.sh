050f99c509dcecd40b57c8f4f4fd0f13eb1d9eb6
Gustavo S Frehse <gusfrehse@gmail.com>
2023-11-18 17:11:16 -0300

    asdf

--- run.sh
+++ run.sh
@@ -2,18 +2,22 @@
 
 tries=10
 
-cmd="mpirun -n $n ./tsp"
 
-for n in 1 2 3 4
+for p in 1 2 3 4
 do
-    printf "$n"
-    for i in {0..10}
+    echo $p
+    for n in 10 11 12 13 14 15 16 17 18 19 20
     do
-        /usr/bin/time -f %e -o tempo $cmd < tsp.in > /dev/null
-        TEMPO=$(cat ./tempo)
-        rm ./tempo
-        printf ", $TEMPO"
+        printf "$n"
+        cmd="mpirun -n $p ./tsp"
+        cmd="echo $p"
+        for i in {0..10}
+        do
+            /usr/bin/time -f %e -o tempo $cmd < tsp$n.in > /dev/null
+            TEMPO=$(cat ./tempo)
+            rm ./tempo
+            printf ", $TEMPO"
+        done
+        printf "\n"
     done
-    printf "\n"
 done
-
